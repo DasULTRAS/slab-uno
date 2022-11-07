575c728a0a513a80e56451a40a45248a1e585c75
DasULTRAS <christian@hoelter.ch>
2022-11-08 00:08:04 +0100

    added card pack and some tests

--- sketch.js
+++ sketch.js
@@ -1,5 +1,25 @@
+let cards;
+
+/**
+ * function that loads content before showing site
+ */
+function preload() {
+    cards = loadImage('img/default/cards.png');
+}
+
+/**
+ * setup that loads on start
+ */
 function setup() {
+    // creates a working ground
     createCanvas(windowWidth, windowHeight);
+
+    // grey Background
+    background(51);
+
+    // Top-left corner of the img is at (0, 0)
+    // Width and height are the img's original width and height
+    image(cards, 0,0, windowWidth,windowHeight, 0,0, 86,128);
 }
 
 function draw() {
@@ -11,6 +31,9 @@ function draw() {
     ellipse(mouseX, mouseY, 80, 80);
 }
 
+/**
+ * Is called if window will resized
+ */
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
