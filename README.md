69f4c20aa0c4ff4b73a5aa0183dd768a147aaf54
DasULTRAS <christian@hoelter.ch>
2022-11-14 11:41:28 +0100

    commentar

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # SLAB-UNO
 
 An UNO that aims to be a browser based multiplayer GAME.
+Written in [p5.js](https://github.com/processing/p5.js/wiki/p5.js-overview) and [js](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/).
 
 ## Milestones
 
-- [ ] Karten einbinden (WEBP/SVG/PNG)
+- [ ] Karten einbinden (SVG)
 - [ ] Karten in Stack speichern (Verteilstapel), verteilen (an Spieler) und ablegen (auf den Ablegstapel)
 - [ ] Multiplayer (netzwerk basiert)
--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let card, metaData, bg;
 /**
  * function that loads content before showing site
+ * while this function is loading div 'p5_loading' will be shown
+ * https://github.com/processing/p5.js/wiki/p5.js-overview#loading-screen
  */
 function preload() {
     // Top-left corner of the img is at (0, 0)
@@ -19,6 +21,8 @@ function setup() {
 
     // grey Background
     background(bg);
+
+    /* TESTS */
     card.display(50, 50, 250, 389.6, color(255, 204, 0));
     console.log(metaData["textures"]);
     console.log("width: " + metaData.textures.cards.width);
