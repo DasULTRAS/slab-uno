b6366f2dc10e6f6e93224bb278768619499f6a05
DasULTRAS <christian@hoelter.ch>
2022-11-15 14:35:11 +0100

    Card width

--- card.js
+++ card.js
@@ -4,13 +4,15 @@ class Card {
      * @param {*} img The source image
      * @param {Number} cNum The card Number as int (0-12) the three colored special cards are included
      * @param {Number} color The card color as int (0-3)
+     * @param {Number} sCardWidth
+     * @param {Number} sCardHeight
      */
-    constructor(img, cNum, color){
+    constructor(img, cNum, color, cardWidth, cardHeight){
         this.img = img;
         this.cNum = cNum;
         this.color = color;
-        this.sCardWidth = this.img.width/14;
-        this.sCardHeight = this.img.height/8;
+        this.sCardWidth = cardWidth;
+        this.sCardHeight = cardHeight;
     }
 
     /**
@@ -20,10 +22,10 @@ class Card {
      */
     display(x, y){
         //image(source, x, y, width, height, cutting-x, cutting-y, cutting-width, cutting-height)
-        
-        image(this.img, x, y, 
-            this.sCardWidth, this.sCardHeight, 
-            this.sCardWidth*this.cNum, this.sCardHeight*this.color, 
+
+        image(this.img, x, y,
+            this.sCardWidth, this.sCardHeight,
+            this.sCardWidth*this.cNum, this.sCardHeight*this.color,
             this.sCardWidth, this.sCardHeight);
     }
 }
