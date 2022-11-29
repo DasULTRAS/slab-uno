c2ee4f80cd22b917473f2c527d061027d61ceba6
Bigler <mbigler@herbrand.rz.evosec.de>
2022-11-29 15:24:02 +0100

    Add Game.js as game instance

---
+++ slab-uno/src/common/game.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./game.css"
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+export default Game;
