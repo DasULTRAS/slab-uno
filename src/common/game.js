d1758cb4fcfb4ae9c2665ce034f1a29b1f4a3f86
RussiaPlayer <wiskystar2@gmail.com>
2022-12-04 17:28:20 +0100

    Add background to game

--- src/common/game.js
+++ src/common/game.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./game.css"
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div>Test</div>
-        );
-    }
+function Game () {
+    return (
+        <div class="background">
+            
+        </div>
+    );
 }
 
 export default Game;
