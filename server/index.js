fb5f2b8bfa62a33f3c7f3becafebe22db9f82469
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2022-12-06 20:27:43 +0100

    erste socket.io tests

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const {Server} = require("socket.io");
 const cors = require("cors");
+const {render} = require("express/lib/application");
 
 app.use(cors());
 
@@ -13,6 +14,16 @@ const io = new Server(server, {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("send_lobby_id", (data) => {
+        console.log(`${socket.id}: ${data}`);
+
+        socket.broadcast.emit("receive_lobby_id", data);
+    })
 });
 
 server.listen(3003, () => {
