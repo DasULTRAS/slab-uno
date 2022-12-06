bb5205546db7f0fb26c26a608c0c2efc421e3e69
DasULTRAS <christian@hoelter.ch>
2022-12-06 15:31:20 +0100

    edit lobby design

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import Lobby from "./common/lobby";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Game/>
+        <Lobby/>
     </React.StrictMode>
 );
