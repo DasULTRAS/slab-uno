bc830b187513429a243317b52820c4ce99449955
DasULTRAS <christian@hoelter.ch>
2022-12-06 14:01:42 +0100

    lobby anfang

---
+++ src/common/lobby.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./lobby.css"
+
+function Lobby() {
+    return (
+        <div className="content">
+            <button className="createLobbyButton">create new Lobby</button>
+        </div>
+    );
+}
+
+export default Lobby;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Game from "./common/game";
+import Lobby from "./common/lobby";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Game/>
+        <Lobby/>
     </React.StrictMode>
 );
