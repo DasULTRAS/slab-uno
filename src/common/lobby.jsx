38b46355d16c70538d2236a171adc5214b8fe6a3
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2022-12-06 18:42:27 +0100

    lobby id input angepasst

--- src/common/lobby.jsx
+++ src/common/lobby.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./lobby.css";
 import logo from "../assets/UNO_Button.png";
 
+
 function Lobby() {
     return (
         <>
             <img className="logo" src={logo}/>
             <div className="content">
-                <textarea className="lobbyCode"></textarea>
+                <div className="lobbyID">
+                    <label>Lobby ID: </label>
+                    <input></input>
+                </div>
                 <div className="buttons">
                     <button className="createLobbyButton">create new Lobby</button>
                     <button className="joinLobbyButton">join Lobby</button>
