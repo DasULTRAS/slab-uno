2854bc7a5fda5246b6284121cbd6fab6187f6a9e
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2022-12-07 22:45:26 +0100

    Created simple lobby system.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Lobby from "./common/lobby";
+import Game from "./common/game";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
