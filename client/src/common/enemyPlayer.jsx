e25bdc27d91d71f2a6ab667282cf3e9ab10b4d55
RussiaPlayer <wiskystar2@gmail.com>
2022-12-08 20:59:42 +0100

    Add enemyPlayer design (need some work)

--- client/src/common/enemyPlayer.jsx
+++ client/src/common/enemyPlayer.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+import "./enemyPlayer.css";
+
+export default function EnemyPlayer({cardCount, playerName}) {
+    return(
+        <div className="enemyPlayer">     
+            <div className="cardCount">{cardCount}</div>
+            <div className="playerName">{playerName}</div>
+        </div>
+    );
+};
