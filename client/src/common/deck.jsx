fe4f2b8aa8c392fc54b08705f049b60fa64c68f7
RussiaPlayer <wiskystar2@gmail.com>
2022-12-08 20:08:49 +0100

    Add fanstyle for cards (need some work)

--- client/src/common/deck.jsx
+++ client/src/common/deck.jsx
@@ -4,17 +4,36 @@ import Card from "./card";
 
 export default function Deck({cards, cardSize}) {
     const deckLength = cards.length;
+    const curl = Math.pow(deckLength, 1.30) * 10;
     const deg = deckLength > 1 ? -deckLength * 15 : 0;
     let degs = deg / 2;
+    const initialDown = deckLength * 7;
+    let down = initialDown / 2;
+    const initialOver = curl;
+    let over = initialOver / 2;
+
+    console.log(
+        `deckLength: ${deckLength},\n` +
+        `curl: ${curl},\n` +
+        `deg: ${deg},\n` +
+        `degs: ${degs},\n` +
+        `initialDown: ${initialDown},\n` +
+        `down: ${down},\n` +
+        `initialOver: ${initialOver},\n` +
+        `over: ${over},\n`);
 
     function fanStyle(num) {
+        let overHalf = num > (deckLength - 1) / 2;
         if (num > 0) {
             degs -= deg / (deckLength - 1);
+            down -= initialDown / (deckLength - 1);
+            over -= initialOver / (deckLength - 1);
         }
         return {
             'zIndex' : num,
-            transform: `translateX(${3.2 * num}em) 
-            rotate(${degs}deg)` 
+            transform: `translateY(${(overHalf ? -down : down)}%) 
+            translateX(${(-50 + over * -1)}%) 
+            rotate(${degs}deg)`
         }
     }
 
