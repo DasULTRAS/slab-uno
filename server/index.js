8837eabb0f7889ffa6430f788b2da29bc06aecb4
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2022-12-11 19:10:40 +0100

    Improves the room connection

--- server/index.js
+++ server/index.js
@@ -26,16 +26,18 @@ io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
     socket.broadcast.emit('message', {message: `User Connected: ${socket.id}`});
 
-    socket.on("disconnect", (data) => {
+    socket.on("disconnect", () => {
         console.log(`${socket.id} disconnected.`);
-        socket.broadcast.emit('message', {message: `${socket.id} disconnected.`});
         const lobby = lobbyManagement.getLobbyBySocketID(socket.id);
         if (lobby != null) {
             // Remove player from lobby
             lobby.removePlayer(socket.id);
-            if (lobby.players.length == 0)
+            io.to(lobby.lobbyID).emit("message", {message: `Player ${socket} disconnected.`});
+            if (lobby.players.length === 0) {
                 // Remove Lobby if is empty
                 lobbyManagement.removeLobby(lobby.lobbyID);
+                socket.broadcast.emit("message", {message: `Lobby ${lobby.lobbyID} removed.`});
+            }
         }
     });
 });
@@ -47,20 +49,23 @@ io.on("connection", (socket) => {
 
     socket.on("join_lobby", (data) => {
         const lobby = lobbyManagement.joinLobby(data, socket);
-        if (lobby != null) io.emit("player_change", {message: `Player joined: ${data.username}`, lobby: lobby});
+        if (lobby != null) io.to(lobby.lobbyID).emit("player_change", {
+            message: `Player joined: ${data.username}`,
+            lobby: lobby
+        });
     });
 
-    socket.on("ready_to_play", (data) => {
+    socket.on("ready_to_play", () => {
         const player = lobbyManagement.getPlayerBySocketID(socket.id);
         if (player !== null) {
             player.readyToPlay = !player.readyToPlay;
-            socket.emit("message", {message: `Player status changed: ${player.username}`});
-            io.emit("player_change", {
+            io.to(player.lobbyID).emit("message", {message: `Player status changed: ${player.username}`});
+            io.to(player.lobbyID).emit("player_change", {
                 lobby: lobbyManagement.getLobbyByID(player.lobbyID)
             });
         }
     });
-    socket.on("start_game", (data) => {
+    socket.on("start_game", () => {
         const lobby = lobbyManagement.getLobbyBySocketID(socket.id);
         if (lobby != null) {
             // Test if every Player is ready
@@ -70,10 +75,10 @@ io.on("connection", (socket) => {
             })
             if (ready) {
                 // Start the game
-                io.emit("start_game", {message: `Game started.`, lobby: lobby});
-                io.emit("message", {message: `Game in Lobby ${lobby.lobbyID} started.`});
-                // init Deck
-                lobby.deck = new Deck(parseInt(lobby.players.length / 4) + 1);
+                io.to(lobby.lobbyID).emit("start_game", {message: `Game started.`, lobby: lobby});
+                io.to(lobby.lobbyID).emit("message", {message: `Game in Lobby ${lobby.lobbyID} started.`});
+                // init Deck   // Math.trunc returns the number of an float
+                lobby.deck = new Deck(Math.trunc(lobby.players.length / 4) + 1);
             }
         }
     });
