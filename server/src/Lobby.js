d7763d5a410f02d1c7397655eebe1cd4c9f08aae
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2023-01-03 21:40:27 +0100

    Implemented Setting Wild on Wild

--- server/src/Lobby.js
+++ server/src/Lobby.js
@@ -21,7 +21,7 @@ export default class Lobby {
         this.messages = [];
         this.gameSettings = [];
         this.gameSettings.push(new Settings("wild_on_wild", "Wild on Wild", "Is it allow to place a black/wild Card on another black/wild Card?", true));
-        this.gameSettings.push(new Settings("challenge_wild_draw_four", "Challenge Wild Draw Four Card", "If a Wild Draw Four card is laid, it can be challenged. This checks whether the card was the only option. If it was the only possibility, the challenger must draw 6 cards, otherwise the dealer of the Wild Draw Four must draw 4.", false));
+        this.gameSettings.push(new Settings("challenge_wild_draw_four", "Challenge Wild Draw Four Card (not implemented)", "If a Wild Draw Four card is laid, it can be challenged. This checks whether the card was the only option. If it was the only possibility, the challenger must draw 6 cards, otherwise the dealer of the Wild Draw Four must draw 4.", false));
         this.gameSettings.push(new Settings("play_alone", "Play alone", "You can start the Game alone.", false));
     }
 
@@ -73,6 +73,12 @@ export default class Lobby {
                 console.log(card);
                 console.log("WRONG CARD: declared color is not equal played card");
                 return false;
+            } else if (this.playedCards.last.color === card.color){
+                // Wild on Wild
+                if (!this.getSettingByTitle("wild_on_wild").enabled) {
+                    console.log("WRONG CARD: Wild on Wild");
+                    return false;
+                }
             }
         } else if (this.playedCards.last.color !== card.color && this.playedCards.last.type !== card.type) {
             // Colored Card have no matching attribute
